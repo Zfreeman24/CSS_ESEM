e76c0f25e70c85227d0b773b672aef0e60d24cd8
zfreeman3 <zjf13@txstate.edu>
2024-04-15 10:34:59 -0500

    ESEM Implementation

---
+++ ESEM/Laptop_Implementation/FourQ_64bit_and_portable/AMD64/consts.s
@@ -0,0 +1,327 @@
+	.file	"consts.c"
+	.text
+.Ltext0:
+	.data
+	.align 32
+	.type	PRIME1271, @object
+	.size	PRIME1271, 32
+PRIME1271:
+	.quad	-1
+	.quad	9223372036854775807
+	.quad	-1
+	.quad	9223372036854775807
+	.align 32
+	.type	TWOx8, @object
+	.size	TWOx8, 32
+TWOx8:
+	.long	2
+	.long	2
+	.long	2
+	.long	2
+	.long	2
+	.long	2
+	.long	2
+	.long	2
+	.align 32
+	.type	ONEx8, @object
+	.size	ONEx8, 32
+ONEx8:
+	.long	1
+	.long	1
+	.long	1
+	.long	1
+	.long	1
+	.long	1
+	.long	1
+	.long	1
+	.text
+.Letext0:
+	.file 1 "/usr/include/x86_64-linux-gnu/bits/types.h"
+	.file 2 "/usr/include/x86_64-linux-gnu/bits/stdint-uintn.h"
+	.file 3 "AMD64/consts.c"
+	.section	.debug_info,"",@progbits
+.Ldebug_info0:
+	.long	0xeb
+	.value	0x4
+	.long	.Ldebug_abbrev0
+	.byte	0x8
+	.uleb128 0x1
+	.long	.LASF15
+	.byte	0xc
+	.long	.LASF16
+	.long	.LASF17
+	.long	.Ldebug_line0
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x8
+	.long	.LASF0
+	.uleb128 0x2
+	.byte	0x2
+	.byte	0x7
+	.long	.LASF1
+	.uleb128 0x2
+	.byte	0x4
+	.byte	0x7
+	.long	.LASF2
+	.uleb128 0x2
+	.byte	0x8
+	.byte	0x7
+	.long	.LASF3
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x6
+	.long	.LASF4
+	.uleb128 0x2
+	.byte	0x2
+	.byte	0x5
+	.long	.LASF5
+	.uleb128 0x3
+	.byte	0x4
+	.byte	0x5
+	.string	"int"
+	.uleb128 0x4
+	.long	.LASF7
+	.byte	0x1
+	.byte	0x2a
+	.byte	0x16
+	.long	0x2b
+	.uleb128 0x2
+	.byte	0x8
+	.byte	0x5
+	.long	.LASF6
+	.uleb128 0x4
+	.long	.LASF8
+	.byte	0x1
+	.byte	0x2d
+	.byte	0x1b
+	.long	0x32
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x6
+	.long	.LASF9
+	.uleb128 0x4
+	.long	.LASF10
+	.byte	0x2
+	.byte	0x1a
+	.byte	0x14
+	.long	0x4e
+	.uleb128 0x4
+	.long	.LASF11
+	.byte	0x2
+	.byte	0x1b
+	.byte	0x14
+	.long	0x61
+	.uleb128 0x5
+	.long	0x74
+	.long	0x9c
+	.uleb128 0x6
+	.long	0x32
+	.byte	0x7
+	.byte	0
+	.uleb128 0x7
+	.long	.LASF12
+	.byte	0x3
+	.byte	0xc
+	.byte	0xa
+	.long	0x8c
+	.uleb128 0x9
+	.byte	0x3
+	.quad	ONEx8
+	.uleb128 0x7
+	.long	.LASF13
+	.byte	0x3
+	.byte	0xd
+	.byte	0xa
+	.long	0x8c
+	.uleb128 0x9
+	.byte	0x3
+	.quad	TWOx8
+	.uleb128 0x5
+	.long	0x80
+	.long	0xd8
+	.uleb128 0x6
+	.long	0x32
+	.byte	0x3
+	.byte	0
+	.uleb128 0x7
+	.long	.LASF14
+	.byte	0x3
+	.byte	0xe
+	.byte	0xa
+	.long	0xc8
+	.uleb128 0x9
+	.byte	0x3
+	.quad	PRIME1271
+	.byte	0
+	.section	.debug_abbrev,"",@progbits
+.Ldebug_abbrev0:
+	.uleb128 0x1
+	.uleb128 0x11
+	.byte	0x1
+	.uleb128 0x25
+	.uleb128 0xe
+	.uleb128 0x13
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x1b
+	.uleb128 0xe
+	.uleb128 0x10
+	.uleb128 0x17
+	.byte	0
+	.byte	0
+	.uleb128 0x2
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.byte	0
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0x8
+	.byte	0
+	.byte	0
+	.uleb128 0x4
+	.uleb128 0x16
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x39
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0x5
+	.uleb128 0x1
+	.byte	0x1
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x1
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0x6
+	.uleb128 0x21
+	.byte	0
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x2f
+	.uleb128 0xb
+	.byte	0
+	.byte	0
+	.uleb128 0x7
+	.uleb128 0x34
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x39
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x3f
+	.uleb128 0x19
+	.uleb128 0x2
+	.uleb128 0x18
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"",@progbits
+	.long	0x1c
+	.value	0x2
+	.long	.Ldebug_info0
+	.byte	0x8
+	.byte	0
+	.value	0
+	.value	0
+	.quad	0
+	.quad	0
+	.section	.debug_line,"",@progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",@progbits,1
+.LASF2:
+	.string	"unsigned int"
+.LASF7:
+	.string	"__uint32_t"
+.LASF14:
+	.string	"PRIME1271"
+.LASF3:
+	.string	"long unsigned int"
+.LASF16:
+	.string	"AMD64/consts.c"
+.LASF11:
+	.string	"uint64_t"
+.LASF15:
+	.ascii	"GNU C17 9.3.0 -march=znver2 -mmmx -mno-3dnow -msse -msse2 -m"
+	.ascii	"sse3 -mssse3 -msse4a -mcx16 -msahf -mmovbe -maes -msha -mpcl"
+	.ascii	"mul -mpopcnt -mabm -mno-lwp -mfma -mno-fma4 -mno-xop -mbmi -"
+	.ascii	"mno-sgx -mbmi2 -mno-pconfig -mno-wbnoinvd -mno-tbm -mavx -ms"
+	.ascii	"se4.2 -msse4.1 -mlzcnt -mno-rtm -mno-hle -mrdrnd -mf16c -mfs"
+	.ascii	"gsbase -mrdseed -mprfchw -madx -mfxsr -mxsave -mxsaveopt -mn"
+	.ascii	"o-avx512f -mno-avx512er -mno-avx512cd -mno-avx512pf -mno-pre"
+	.ascii	"fetchwt1 -mclflushopt -mxsavec -mxsaves -mno-avx512dq -mno-a"
+	.ascii	"vx512bw -mno-avx512vl -mno-avx512ifma -mno-avx512vbmi -mno-a"
+	.ascii	"vx5124fmaps -mno-avx5124vnniw -mclwb -mno-mwaitx -mclzero -m"
+	.ascii	"no-pku -mrdpid -mno-gfni -mshstk -mno-avx512vbmi2 -mno-avx51"
+	.ascii	"2vnni -mvaes -mvpclmulqdq -mno-avx512bitalg -mno-avx512vpopc"
+	.ascii	"ntdq -mno-movdiri -mno-movdir64b -mno-waitpkg -mno-cl"
+	.string	"demote -mno-ptwrite --param l1-cache-size=32 --param l1-cache-line-size=64 --param l2-cache-size=512 -mtune=znver2 -mavx2 -g -O3 -O3 -fwrapv -fomit-frame-pointer -fasynchronous-unwind-tables -fstack-protector-strong -fstack-clash-protection -fcf-protection"
+.LASF12:
+	.string	"ONEx8"
+.LASF0:
+	.string	"unsigned char"
+.LASF9:
+	.string	"char"
+.LASF10:
+	.string	"uint32_t"
+.LASF6:
+	.string	"long int"
+.LASF13:
+	.string	"TWOx8"
+.LASF1:
+	.string	"short unsigned int"
+.LASF4:
+	.string	"signed char"
+.LASF17:
+	.string	"/mnt/c/Users/Zach Freeman/Desktop/CSS_ESEM/ESEM/Laptop_Implementation/FourQ_64bit_and_portable"
+.LASF5:
+	.string	"short int"
+.LASF8:
+	.string	"__uint64_t"
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
